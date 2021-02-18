c80ce1c04e8c380f67b56db25c2c238c1e164b2f
chenxiongji <chenxiongji@idaoben.com>
2021-02-18 17:25:33 +0800

    update shell

--- onekey/recompile.sh
+++ onekey/recompile.sh
@@ -19,7 +19,7 @@ echo
 clear
 
 rm -Rf openwrt/common openwrt/files openwrt/devices
-svn co https://github.com/sarabear/Actions-OpenWrt/trunk/devices openwrt/devices
+svn co https://github.com/sarabear/OpenWrt/trunk/devices openwrt/devices
 cd openwrt
 
 [ $(grep '^CONFIG_TARGET.*DEVICE.*=y' .config | sed -r 's/.*DEVICE_(.*)=y/\1/') == generic ] && {
